09e3148d5363e9048e94bffc1b83d83c404d5450
Rahil Ansari <rahilansari261@gmail.com>
2025-09-01 19:26:14 +0530

    Update package-lock.json to include hasInstallScript property for improved package management

---
+++ prisma/migrations/20250901135356_/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `schools` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `contact` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `email_id` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
